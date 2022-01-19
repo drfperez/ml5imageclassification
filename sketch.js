a2c2c4da783f89472bfc9e1e6f27c37520599b4e
Francisco Pérez <drfperez@gmail.com>
2022-01-19 13:45:24 +0100

    Update sketch.js
    Labels and rounding

--- sketch.js
+++ sketch.js
@@ -19,14 +19,26 @@ function gotResults(error, results) {
   } else {
     console.log(results);
     let label = results[0].className;
-    let prob = results[0].probability;
+    let prob1 = results[0].probability;
+    let label2 = results[1].className;
+    let prob21 = results[1].probability;
+    
+    //round(x,4) 
+    let prob = precise(prob1,2)
+    let prob2 = precise(prob21,2)
     fill(0);
-    textSize(64);
-    text(label, 10, height - 100);
-    createP(label);
-    createP(prob);
+    textSize(32) ;
+    text(label, 10, height - 50);
+  
+    createP("According to mobilnet this image contains a"+label+" with the highest probability of "+prob);
+     createP("A second possibility is "+label2+" with the a probability of "+prob2);
   }
 }
+function precise(x) {
+  return 100*Number.parseFloat(x).toPrecision(4)+" %";
+}
+
+
 
 function imageReady() {
   image(puffin, 0, 0, width, height);
@@ -34,7 +46,7 @@ function imageReady() {
 
 function setup() {
   createCanvas(640, 480);
-  puffin = createImg('images/puffin.jpg', imageReady);
+  puffin = createImg('images/joe.jpg', imageReady);
   puffin.hide();
   background(0);
   mobilenet = ml5.imageClassifier('MobileNet', modelReady);
